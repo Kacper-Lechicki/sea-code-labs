619d4bd581ddd6a3a5d47cc36d62cfe6553865aa
Kacper Lechicki <klechicki.code@gmail.com>
2024-11-20 14:52:45 +0100

    [TEST-0] test: commitlint test 2

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,10 +4,10 @@ module.exports = {
       rules: {
         'custom-header': (parsed) => {
           const { header } = parsed;
-          const regex = /^\[([A-Z]+-\d+)] (fix|feat|docs|refactor): (.+)$/;
+          const regex = /^\[([A-Z]+-\d+)] (fix|feat|docs|refactor|test): (.+)$/;
 
           if (!regex.test(header)) {
-            return [false, `Commit message must follow format: [JIRA-123] fix|feat|docs|refactor: description`];
+            return [false, `Commit message must follow format: [JIRA-123] fix|feat|docs|refactor|test: description`];
           }
 
           return [true];
